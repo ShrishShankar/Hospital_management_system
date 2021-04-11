f3102760e3ab29bf6e7bda2a1c28fad50b378d87
Shrish Shankar <shrishshankar11@gmail.com>
2021-04-11 23:11:38 +0530

    Change config settings'

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'hospitalmanagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'hospital',
         'USER': 'root',
         'PASSWORD': 'root',
